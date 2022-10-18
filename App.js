bea6da2475de27d8bbd94629ca47fe1e1b4e00c1
Konsta <n0jako00@students.oamk.fi>
2022-10-18 17:58:51 +0300

    yritystä

--- App.js
+++ App.js
@@ -1,20 +1,113 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, ScrollView, Pressable, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {styles } from './styles/styles.js';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+// Author: Konsta Jäske
 
 export default function App() {
+
+  let board = [];
+  const NBR_OF_DICES = 6;
+  const NBR_OF_THROWS = 3;
+
+  const [nbrOfThrowsLeft, setNbrOfThrowsLeft] = useState(NBR_OF_THROWS);
+  const [status, setStatus] = useState("");
+  const [selectedDices, setSelectedDices] =
+    useState(new Array(NBR_OF_DICES).fill(false));
+
+    useEffect(() => {
+      checkWinner();
+      if (nbrOfThrowsLeft === NBR_OF_THROWS) {
+        setStatus('Game has not started');
+      }
+      if (nbrOfThrowsLeft < 0) {
+        setNbrOfThrowsLeft(NBR_OF_THROWS-1);
+      }
+    }, [nbrOfThrowsLeft]);
+
+  function getDiceColor(i) {
+    if (board.every((val, i, arr) => val === arr[0])) {
+      return "orange";
+    }
+    else {
+      return selectedDices[i] ? "black" : "steelblue";
+    }
+  }
+
+  function selectDice(i) {
+    let dices = [...selectedDices];
+    dices[i] = selectedDices[i] ? false : true;
+    setSelectedDices(dices);
+  }
+
+  function throwDices() {
+    for (let i = 0; i < NBR_OF_DICES; i++) {
+      if (!selectedDices[i]) {
+        let randomNumber = Math.floor(Math.random() * 6 + 1);
+        board[i] = 'dice-' + randomNumber;
+      }
+    }
+    setNbrOfThrowsLeft(nbrOfThrowsLeft-1);
+  }
+
+  function checkWinner() {
+    if (board.every((val, i, arr) => val === arr[0]) && nbrOfThrowsLeft > 0) {
+      setStatus('You won');
+    }
+    else if (board.every((val, i, arr) => val === arr[0]) && nbrOfThrowsLeft === 0) {
+      setStatus('You won, game over');
+      setSelectedDices(new Array(NBR_OF_DICES).fill(false));
+    }
+    else if (nbrOfThrowsLeft === 0) {
+      setStatus('Game over');
+      setSelectedDices(new Array(NBR_OF_DICES),fill(false));
+    }
+    else {
+      setStatus('Keep on throwing');
+    }
+  }
+
+  
+
+  const row = [];
+  for (let i = 0; i < NBR_OF_DICES; i++) {
+    row.push(
+      <Pressable
+        key={"row" + i}
+        onPress={() => selectDice(i)}>
+      <MaterialCommunityIcons
+        name={board[i]}
+        key={"row" + i}
+        size={50}
+        color={getDiceColor(i)}>
+        </MaterialCommunityIcons>
+      </Pressable>
+    );
+  }
+
+  const [bonus, setBonus] = useState(0)
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView style={styles.container}>
+    <View>
+      <View style={styles.flex}>{row}</View>
+      <Text style={styles.header}>Mini-Yahtzee</Text>
+      <Text>Throws left: {nbrOfThrowsLeft}</Text>
+      <Text>{status}</Text>
+      <Pressable style={styles.button}
+      onPress={() => throwDices()}>
+        <Text style={styles.buttonText}>
+          Throw dices
+        </Text>
+      </Pressable>
+      <Text style={styles.totalText}>Total:</Text>
+      <Text style={styles.text}>You are {bonus} points away from bonus</Text>
+      <Text style={styles.footer}>Author: Konsta Jäske</Text>
     </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/styles.js
@@ -0,0 +1,68 @@
+// Author: Konsta Jäske, TIK21KM
+import { StyleSheet, StatusBar } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: StatusBar.currentHeight || 100
+  },
+  text: {
+    padding: 10,
+  },
+  totalText: {
+    fontSize: 30,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  textInput: {
+    backgroundColor: '#d9d9d7',
+  },
+  header: {
+    FontSize: 30,
+    backgroundColor: 'skyblue',
+    textAlign: 'center'
+  },
+  footer: {
+    MarginTOp: 10,
+    backgroundColor: 'skyblue',
+    textAlign: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 23,
+    textAlign: 'center',
+    margin: 10,
+  },
+  author: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+  },
+
+  item: {
+    margin: 15,
+    padding: 5
+  },
+  flex: {
+    flexDirection: "row"
+  },
+  button: {
+    backgroundColor: '#86fcb8',
+    width: 150
+  },
+  buttonText: {
+    color:"#2B2B52",
+    fontSize: 20
+  },
+  result: {
+    fontSize: 20,
+    marginLeft: 170,
+    marginRight: 170,
+    textAlign: 'center'
+  }
+});
